eee46dc0aae75b90cfff5dc7488fe8379e0e3c4f
kanga-prog <9952@holbertonstudents.com>
2024-12-11 15:32:41 +0100

    12-binary_tree_leaves.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - Measures the depth of a node in a binary tree.
+ * @tree: A pointer to the node to measure the depth.
+ *
+ * Return: The depth of the node, or 0 if the tree is NULL.
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	while (tree && tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
+
